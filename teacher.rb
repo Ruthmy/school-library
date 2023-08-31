f0cce5cfb60a43149fbdefd9323c1ad4fa20e3af
Ruthmy <ruthmy17@gmail.com>
2023-08-30 21:28:38 -0600

    Create 'teacher.rb' -file- - Created the class 'teacher' that inherits from the class person.

---
+++ teacher.rb
@@ -0,0 +1,14 @@
+require_relative 'person'
+
+class Teacher < Person
+  attr_accessor :specialization
+
+  def initialize(specialization, age, name: 'Unknown', parent_permision: true)
+    super(age, name, parent_permision)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
